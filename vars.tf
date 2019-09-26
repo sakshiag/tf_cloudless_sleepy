6459e4bb3499cfceaab1870a03e657ab5079a277
Sakshi Agarwal <sakshiag@in.ibm.com>
2019-09-26 10:12:59 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
 # Configure these variables
 
-variable "sample_var {
+variable "sample_var_not_master" {
   description = "A sample_var to pass to the template."
   default     = "hello"
 }
 
-variable "sleepy_time" {
+variable "sleepy_time_not_master" {
   description = "How long our local-exec will take a nap."
   default     = 0
 }
