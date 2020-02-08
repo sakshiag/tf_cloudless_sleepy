44e788020a5465c68dbfe2341cb9b5d3d806add4
Sakshi Agarwal <sakshiag@in.ibm.com>
2020-02-08 15:00:16 +0530

    Update main.tf

--- testFolder/main.tf
+++ testFolder/main.tf
@@ -1,6 +1,6 @@
 # Create an arbitrary local resource
 data "template_file" "test_folder" {
-  template = "Hello, I am a template. My sample_var value = $${sample_var_branch}"
+  template = "Hello, I am a template. My sample_var value = $${sample_var}"
 
   vars {
     sample_var = "${var.sample_var_branch}"
