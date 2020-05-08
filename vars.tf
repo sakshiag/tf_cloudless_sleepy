f12fa1759931621497bac0697aef2bca97ab7beb
saki19 <saki19@users.noreply.github.com>
2020-05-08 16:24:46 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -16,3 +16,13 @@ variable "sample_var_list" {
   type = "list"
   default     = ["test1","test2"]
 }
+
+variable "sample_var_map" {
+  description = "How long our local-exec will take a nap."
+  type = "map"
+  default = {
+    us-east-1 = "image-1234"
+    us-west-2 = "image-4567"
+  }
+}
+
